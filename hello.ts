dc8155fbba227100d8b4dcc979983ed7161310a7
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2024-05-05 17:55:55 -0500

    installed tsx for debugging

---
+++ hello.ts
@@ -0,0 +1,6 @@
+function hello(person) {
+    return "hello, " + person;
+}
+let user = 'Brendan';
+const result = hello(user);
+console.log('result', result);
--- src/kaboomCtx.ts
+++ src/kaboomCtx.ts
@@ -1,8 +1,12 @@
 import kaboom from "kaboom";
+import { scale } from "./constants";
 
 export const k = kaboom({
     width: 256,
     height: 144,
+    scale,
     letterbox: true,
     global: false,
-})
+});
+
+console.log(k.scale);
